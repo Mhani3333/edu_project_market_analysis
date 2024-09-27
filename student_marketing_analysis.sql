70aeb9f664fe78f23fa7e26cd7d02f5c31587138
Mhani3333 <Mohammedhani3333@gmail.com>
2024-09-27 10:00:23 +0300

    Cities and Studying fields
    simplifying code, adding ratios for students per cities and preferred studying fields.

--- student_marketing_analysis.sql
+++ student_marketing_analysis.sql
@@ -1,48 +1,63 @@
-/* Adding two CTEs, TotalCounts for total number of students
-and CountryCounts for students from each country,
-second CTE includes a row for all nationality grouped as "Other" */
-WITH TotalCounts AS (
-    SELECT COUNT(*) AS total_students
-    FROM students
-),
-CountryCounts AS (
-    SELECT 'Other' AS nationality, COUNT(*) AS student_count
+WITH TotalCounts AS
+	( SELECT COUNT(*) AS total_students
+    FROM students ),
+CountryCounts AS
+	( SELECT 'Other' AS nationality, COUNT(*) AS student_count
     FROM students
     WHERE nationality != 'Saudi Arabia'
     UNION ALL
     SELECT nationality, COUNT(*) AS student_count
     FROM students
-    GROUP BY nationality
-)
-/*Then combining both CTEs to get ratio of (students from each country:total students)*/
+    GROUP BY nationality )
 SELECT 
     nationality,
-    ROUND((student_count * 100.0 / total_students), 2) AS percentage
+    ROUND(student_count * 100.0 / total_students) AS nationality_ratio
 FROM CountryCounts, TotalCounts
 ORDER BY student_count DESC;
 
 
-/* Next step is knowing the ratio of gender and age frames, to target audience more effiently*/
 
-/*Forming CTE of how many male or female student in each age frame (-17 or 17-24 or +24) */
 WITH GenderAge AS 
-	(
-	SELECT gender, 
+	( SELECT gender, 
 	CASE WHEN EXTRACT(YEAR FROM AGE(current_date, date_of_birth)) BETWEEN 17 AND 24 THEN '17-24'
 	WHEN EXTRACT (YEAR FROM AGE(current_date, date_of_birth)) > 24 THEN '+24'
 	ELSE '-17' 
 	END AS age_frame,
 	COUNT (*) AS count_age
-FROM students
-GROUP BY gender, age_frame),
-/* Then forming another CTE of total number of students */	
-	TotalCounts AS 
-	(
+	FROM students
+	GROUP BY gender, age_frame ),
+TotalCounts AS (
     SELECT COUNT(*) AS total_students
-    FROM students
-	)
-/* Finally combinig both CTEs to get the ratio of (how many male or female in each age frame:total students) */	
+    FROM students )
 SELECT gender, age_frame, 
-	ROUND((count_age*100/total_students), 2) AS percentage
+	ROUND((count_age*100/total_students)) AS gender_age_ratio
 FROM GenderAge, TotalCounts
-ORDER BY percentage DESC
+ORDER BY gender_age_ratio DESC
+
+
+
+WITH TotalCounts AS (
+    SELECT COUNT(*) AS total_students
+    FROM students ),
+students_in_city AS (
+	SELECT city_of_residence, count (*) AS stu_count
+	FROM students
+	GROUP BY city_of_residence )
+SELECT city_of_residence,
+	ROUND(stu_count*100/total_students) AS cities_ratio
+FROM students_in_city, TotalCounts
+ORDER BY cities_ratio DESC
+
+
+
+WITH TotalCounts AS (
+    SELECT COUNT(*) AS total_students
+    FROM students ),
+StudyFields AS (
+	SELECT desired_studying_field, count(*) AS fields_count
+	FROM students
+	GROUP BY desired_studying_field )
+SELECT desired_studying_field,
+	ROUND(fields_count*100/total_students) AS fields_ratio
+FROM TotalCounts, StudyFields
+ORDER BY fields_ratio DESC
